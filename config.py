4981b12d85967c653a49c0b922ffa5cdeb4627d1
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-07-18 04:26:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ from logging.handlers import RotatingFileHandler
 
 
 # token
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6515426684:AAF8VimPWiDfpeWWyMkqw26snbFCVTSnaXM")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6776572844:AAEnthlAAl2_8b1WQ0oDQiav07tzw38_Njg")
+ # "6515426684:AAF8VimPWiDfpeWWyMkqw26snbFCVTSnaXM"
 
 # api id
 API_ID = int(os.environ.get("APP_ID", "29478593"))
@@ -14,7 +15,7 @@ API_ID = int(os.environ.get("APP_ID", "29478593"))
 API_HASH = os.environ.get("API_HASH", "24c3a9ded4ac74bab73cbe6dafbc8b3e")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002092954715"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002166025521"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "6259443940"))
@@ -27,7 +28,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://knight_rider:GODGURU12345
 DB_NAME = os.environ.get("DATABASE_NAME", "paradoXstr")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002092954715"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002149105272"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -36,7 +37,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 admins_list = "6259443940"
 
 # Start message
-START_MSG = os.environ.get("START_MESSAGE", "Hᴇᴍʟᴏ {first}\n\nᴛʜɪs ɪs ᴀ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇ sᴛᴏʀᴀɢᴇ ʙᴏᴛ.\n\nClick About Us to know more.")
+START_MSG = os.environ.get("START_MESSAGE", "Hᴇᴍʟᴏ {first}\n\nᴛʜɪs ɪs ᴀ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇ sᴛᴏʀᴀɢᴇ ʙᴏᴛ sᴛᴇʀɴʀɪᴛᴛᴇʀ.\n\nClick More Info to know more")
 
 # Retrieve admin IDs from environment variables, if any, otherwise use defaults
 admins_from_env = os.environ.get("ADMINS", admins_list)
@@ -47,19 +48,20 @@ except ValueError:
     raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hemlo {first}\n\n<b>ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴀɴʏ ᴀɴɪᴍᴇ ғɪʟᴇ, Yᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ɪn ᴏᴜʀ ᴀɴɪᴍᴇ ɢᴄ & ᴄʜᴀᴛ ɢᴄ to ᴜsᴇ ᴍᴇ\n\n Jᴏɪɴ ᴛʜᴇ ɢɪᴠᴇɴ ᴄʜᴀɴɴᴇʟ & ɢʀᴏᴜᴘ ᴄʜᴀᴛ</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "Caption - ParadoX")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "Aɴɪᴍᴇ Fɪʟᴇs ʙʏ : @AnimePlaza_STR \n
+ @OtakusMotel_STR")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", "True") == 'True'
 
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+BOT_STATS_TEXT = "<b>ʙᴏᴛ ᴜᴘᴛɪᴍᴇ</b>⥤{uptime}"
+USER_REPLY_TEXT = "You are not authorised! "
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6259443940)
