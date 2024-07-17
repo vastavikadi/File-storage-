62a8259074216774ad28611b5286ccff44257f57
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-07-18 05:24:17 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,28 +9,31 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"        "┏━━━━━━━━━━━━━━━━━┓\n"
-        "ㅤㅤ   ㅤ  <a href='https://t.me/official_str'>sᴛᴇʀɴʀɪᴛᴛᴇʀ</a>ㅤㅤㅤㅤㅤ\n"
-        "┗━━━━━━━━━━━━━━━━━┛\n\n"
-        "╔──────\n"
-        "╟ Owner: <a href='https://t.me/aryan_kadam>Aryan Kadam</a>\n"
-        "╚──────\n"
-        "╔──────\n"
-        "╟ <a href='https://t.me/animeplaza_str'>Anime Plaza STR</a>\n"
-        "╚──────\n"
-        "╔──────\n"
-        "╟ <a href='https://t.me/ongoing_str'>Ongoing Anime</a>\n"
-        "╚──────\n"
-        "╔──────\n"
-        f"╟ Dev: <a href='https://t.me/corpsealone>ɢӈߋʂτ</a>\n"
-        "╚──────",
+            text=(
+                "┏━━━━━━━━━━━━━━━━━┓\n"
+                "ㅤㅤ   ㅤ  <a href='https://t.me/official_str'>sᴛᴇʀɴʀɪᴛᴛᴇʀ</a>ㅤㅤㅤㅤㅤ\n"
+                "┗━━━━━━━━━━━━━━━━━┛\n\n"
+                "╔──────\n"
+                "╟ Owner: <a href='https://t.me/aryan_kadam'>Aryan Kadam</a>\n"
+                "╚──────\n"
+                "╔──────\n"
+                "╟ <a href='https://t.me/animeplaza_str'>Anime Plaza STR</a>\n"
+                "╚──────\n"
+                "╔──────\n"
+                "╟ <a href='https://t.me/ongoing_str'>Ongoing Anime</a>\n"
+                "╚──────\n"
+                "╔──────\n"
+                "╟ Dev: <a href='https://t.me/corpsealone'>ɢӈߋʂτ</a>\n"
+                "╚──────"
+            ),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton("Join Sternritter", url='https://t.me/sternriyal')],
                     [InlineKeyboardButton("🔒 Close", callback_data="close")]
                 ]
-            )
+            ),
+            parse_mode='HTML'  # Ensure 'parse_mode' is set to 'HTML'
         )
     elif data == "close":
         await query.message.delete()
