3211dc9a36526d65e176772f3601173f75e14751
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-07-18 05:22:48 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,7 +9,21 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>ɢӈߋʂτ</a>\n\n○ Source Code : <a href='https://t.me/ghost_kun'>Click here</a>\n○ <a href='https://t.me/corpsealone'>[ᴘᴀʀᴀᴅᴏx]</a>\n○ Support Group : @paradoxdump</b>",
+            text=f"        "┏━━━━━━━━━━━━━━━━━┓\n"
+        "ㅤㅤ   ㅤ  <a href='https://t.me/official_str'>sᴛᴇʀɴʀɪᴛᴛᴇʀ</a>ㅤㅤㅤㅤㅤ\n"
+        "┗━━━━━━━━━━━━━━━━━┛\n\n"
+        "╔──────\n"
+        "╟ Owner: <a href='https://t.me/aryan_kadam>Aryan Kadam</a>\n"
+        "╚──────\n"
+        "╔──────\n"
+        "╟ <a href='https://t.me/animeplaza_str'>Anime Plaza STR</a>\n"
+        "╚──────\n"
+        "╔──────\n"
+        "╟ <a href='https://t.me/ongoing_str'>Ongoing Anime</a>\n"
+        "╚──────\n"
+        "╔──────\n"
+        f"╟ Dev: <a href='https://t.me/corpsealone>ɢӈߋʂτ</a>\n"
+        "╚──────",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
