d00766d3001b44cfd6830079ac5c22fec1e10cb7
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-07-20 12:05:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from pyrogram import Client
 from pyrogram.enums import ParseMode
 from datetime import datetime
 import sys
-from config import API_HASH, API_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT, FORCE_SUB_CHANNEL2
+from config import API_HASH, API_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, CHANNEL_ID, PORT
 
 class Bot(Client):
     def __init__(self):
@@ -21,15 +21,12 @@ class Bot(Client):
             bot_token=TG_BOT_TOKEN
         )
         self.LOGGER = LOGGER
-        self.invitelink1 = None
-        self.invitelink2 = None
 
     async def start(self):
         await super().start()
         usr_bot_me = await self.get_me()
         self.uptime = datetime.now()
-        await self.export_invite_links()
-        
+
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
@@ -58,27 +55,8 @@ class Bot(Client):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
 
-    async def export_invite_links(self):
-        await self.export_invite_link(FORCE_SUB_CHANNEL, 'invitelink1')
-        await self.export_invite_link(FORCE_SUB_CHANNEL2, 'invitelink2')
-
-    async def export_invite_link(self, channel_id, link_attr):
-        if channel_id:
-            try:
-                chat = await self.get_chat(channel_id)
-                link = chat.invite_link
-                if not link:
-                    link = await self.export_chat_invite_link(channel_id)
-                setattr(self, link_attr, link)
-            except Exception as e:
-                self.LOGGER(__name__).warning(e)
-                self.LOGGER(__name__).warning(f"Bot can't Export Invite link from Force Sub Channel {channel_id}!")
-                self.LOGGER(__name__).warning(f"Please double-check the {link_attr} value and make sure the bot is an admin in the channel with Invite Users via Link Permission. Current Force Sub Channel Value: {channel_id}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/paradoxdump for support")
-                sys.exit()
-
 # Initialize the bot
 bot = Bot()
 
-# Ensure the bot starts and sets the invite links
+# Ensure the bot starts
 bot.run()
