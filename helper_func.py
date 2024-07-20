00fd991e107c538853b48d0bbfbeab2204f3ec96
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-07-20 06:31:57 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -5,7 +5,11 @@ import re
 import asyncio
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors import UserNotParticipant
+from pyrogram.enums import ChatMemberStatus
+from config import FORCE_SUB_CHANNEL2
 from config import FORCE_SUB_CHANNEL, ADMINS, FORCE_SUB_CHANNEL2
+from comfig import ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
@@ -32,14 +36,14 @@ async def is_subscribed(filter, client, update):
     if user_id in ADMINS:
         return True
     try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL2, user_id = user_id)
+        member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL2, user_id=user_id)
     except UserNotParticipant:
         return False
 
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
+    if member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
         return True
+    else:
+        return False
 
 async def encode(string):
     string_bytes = string.encode("ascii")
