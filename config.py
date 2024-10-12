181bfe96308e6b901c34952ccbb46fd3cddb48e7
error-auth <auth.2418362@gmail.com>
2024-10-12 14:41:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6259443940"))
 PORT = os.environ.get("PORT", "8080")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://arnavgupta0078:arnav@cluster3301.ojyvd.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "paradoXstr2")
 
 # force sub channel id, if you want enable force sub
